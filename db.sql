89bdd3cf7d02c1e656b9dedcdbe5c60248964f8a
Adrian Cybulski <xeonid@gmail.com>
2014-09-10 16:44:32 +0200

    html view

---
+++ db.sql
@@ -0,0 +1,12 @@
+create table users(id int auto_increment,login text,passwd text,primary key(id));
+
+
+create table attend_username
+
+id int auto_increment,
+subject text,
+when datetime,
+
+create table subject_list_username(id int auto_increment,subject text,wasnt int,percent float);
+
+
---
+++ templates/Account/Login/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wagarowicz| Login</title>
+	<link rel="stylesheet" type="text/css" href="templates/Account/Login/style.css">
+</head>
+<body>
+
+
+
+<div class='login-form'>
+
+
+<form  action='' method='post'>
+
+Login: <input type='text' name='login'><br>
+Hasło: <input type='password' name='passwd'><br>
+<input type='submit' value='zaloguj'>
+
+</form>
+
+</div>
+
+
+</body>
+</html>
