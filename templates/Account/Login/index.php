c41f2a2bc166476311254d7e9ec892b8b9fed53f
Adrian Cybulski <xeonid@gmail.com>
2014-09-11 20:35:47 +0200

    merged layout login page

--- templates/Account/Login/index.php
+++ templates/Account/Login/index.php
@@ -1,27 +1,39 @@
-<!DOCTYPE html>
-<html lang='pl'>
-<head>
-	<meta charset='utf-8'>
-	<title>Wagarowicz| Login</title>
-	<link rel="stylesheet" type="text/css" href="templates/Account/Login/style.css">
-</head>
+ <!doctype html> 
+
+<head> 
+<meta charset="UTF-8">
+<meta name="Description" content="Spierdalaj z lekcji bez obaw ! "> 
+
+<title>Wagarowicz - Zaloguj się ! </title> 
+
+<link rel="stylesheet" type="text/css" href="templates/Account/Login/css/login.css">
+</head> 
+
 <body>
 
+	<div id="main">
+
+		<div id="aboutslider">
+
+		</div>
+
+			<div id="logincontent">
+				<div id="productText"><img src="templates/Account/Login/imagesLogin/Wagarowicz.png"></div>
+					<form id="loginPanel" action="index.php?request=Account&action=login" method="post">
+
+							<p id="loginUsser">Login</p> <input type='text' name='login' id="login"><br>
+							<p id="loginUsser"> Hasło</p> <input type='password' name='passwd' id="passw"><br>
+							<input type='submit' value='Zaloguj' id="loginButon"><br>
+							<a id ="register" href="">Zarejestruj się</a>
 
 
 
+					</form>
 
-<form id='lf' action='index.php?request=Account&action=login' method='post'>
+			</div>
 
-&nbsp;&nbsp;&nbsp;Login: <input id='lf-l' type='text' name='login'><br>
-&nbsp;&nbsp;&nbsp;Hasło: <input id='lf-h' type='password' name='passwd'><br>
-<input type='submit' id='lf-sub' value='zaloguj'>
-<a href='index.php?request=Account&action=register'>Utwórz Konto</a>
+	</div>
 
-</form>
+</body> 
 
-
-
-
-</body>
-</html>
+</html> 
