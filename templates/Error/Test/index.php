eace4d86039aacb3a8fd1a05d30ba85c8539509c
SyclieWhite <sycliewhite@gmail.com>
2014-09-11 21:10:35 +0200

    changed title

---
+++ templates/Error/Test/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Test</title>
+<link rel="stylesheet" type="text/css" href="templates/Error/Test/style.css">
+<title>Test2</title>
+</head>
+<body>
+
+ 	
+
+
+</body>
+</html>
