8e6eac7052e452cab893d3e7b487dbe629c77867
Adrian Cybulski <xeonid@gmail.com>
2014-09-13 11:51:38 +0200

    idnwis

--- mvc/model/modelPlan.php
+++ mvc/model/modelPlan.php
@@ -14,17 +14,30 @@ class modelPlan extends modelAccount
 
 	public function createPlan($pn,$wt,$sr,$cz,$pt)
 	{
-		if(isset($pn) and isset($wt) and isset($sr) and isset($cz) and isset($pt) and $_SESSION['auth'] == true)
+		if($this->auth())
+		{
+			if(isset($pn) and isset($wt) and isset($sr) and isset($cz) and isset($pt))
+			{
+				$l = $_SESSION['login'];
+				$this->db->query("create table plan_".$l."(id int auto_increment,pn text,wt text,sr text,cz text,pt text,primary key(id))");
+
+				$pn = serialize($pn);
+				$wt = serialize($wt);
+				$sr = serialize($sr);
+				$cz = serialize($cz);
+				$pt = serialize($pt);
+				$this->db->exec("insert into plan_".$l." values('NULL','$pt','$wt','$sr','$cz','$pt')");
+
+			}
+		}
+	}
+
+	public function getPlan()
+	{
+		if($this->auth())
 		{
 			$l = $_SESSION['login'];
-			$this->db->query("create table plan_".$l."(id int auto_increment,pn text,wt text,sr text,cz text,pt text,primary key(id))");
-
-			$pn = serialize($pn);
-			$wt = serialize($wt);
-			$sr = serialize($sr);
-			$cz = serialize($cz);
-			$pt = serialize($pt);
-			$this->db->exec("insert into plan_".$l." values('NULL','$pt','$wt','$sr','$cz','$pt')");
+			$handle = $this->db->query("select * from ")
 
 		}
 	}
